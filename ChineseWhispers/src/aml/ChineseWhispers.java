8ebfe9b486f588cb50a745d26e455099c2d823e4
allanlewis99 <allanlewis99@7001370e-0bfd-11df-bf12-379292acd595>
2010-11-10 11:43:50 +0000

    update

--- ChineseWhispers/src/aml/ChineseWhispers.java
+++ ChineseWhispers/src/aml/ChineseWhispers.java
@@ -25,17 +25,34 @@ public class ChineseWhispers {
 	 * 
 	 * @param args
 	 *            Command-line arguments.
-	 * @throws Exception If translation fails.
+	 * @throws Exception
+	 *             If translation fails.
 	 */
 	public static void main(String[] args) throws Exception {
 		// new ChineseWhispers();
 		Translate.setHttpReferrer("http://www.google.co.uk/");
 
-//		String translatedText = Translate.execute("Bonjour le monde",
-//				Language.FRENCH, Language.ENGLISH);
-//		System.out.println(translatedText);
-		
-		
+		int n = 2;
+		String text = "This is a test.";
+		out("Original:\n\t" + text);
+		final Language from = Language.ENGLISH;
+		final Language to = Language.ALBANIAN;
+		for (int i = 0; i < n; i++) {
+			out("Iteration " + (i + 1) + ":\t");
+			text = Translate.execute(text, from, to);
+			out("\t" + text);
+			text = Translate.execute(text, to, from);
+			out("\t" + text);
+		}
+		System.out.println("Finally:\n\t" + text);
 	}
 
+	// public static void main(String[] args) throws Exception {
+	// TranslatorFrame t = new TranslatorFrame();
+	// t.setVisible(true);
+	// }
+
+	private static void out(String s) {
+		System.out.println(s);
+	}
 }
